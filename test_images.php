0b488b7d4e89c608d79552fb6c785cac10e72c54
Abdus Salam <abdussalamseoex@gmail.com>
2026-04-11 10:18:31 +0600

    Cleanup tests

--- test_images.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$posts = \App\Models\Post::latest()->take(3)->get();
-foreach($posts as $p) echo $p->title . " => " . $p->featured_image . "\n";
